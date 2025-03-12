9904ae8d5b4290224556a3fbb679e3251889f1b3
CarloSegreto <carlosegreto95@gmail.com>
2025-03-12 23:10:29 +0100

    Home style up
    -adjusted the navbar to always be over the page content
    -added the books cards
    -added some mock data to do a simulation

--- BookLine-Frontend/src/components/Home/Home.jsx
+++ BookLine-Frontend/src/components/Home/Home.jsx
@@ -2,11 +2,33 @@
 import React, { useState } from "react";
 import NavBar from "../Navbar/Navbar";
 import Filter from '../Navbar/Filter/Filter';
+import BookCard  from "./BookCards/BookCard";
 import './Home.css'; // Import the CSS file for styling
 
 const Home = () => {
   const [showFilter, setShowFilter] = useState(false);
   const [filters, setFilters] = useState({ author: '', year: '', genre: '' });
+
+
+
+  const books = [
+    {
+      cover: 'https://marketplace.canva.com/EAFPHUaBrFc/1/0/1003w/canva-black-and-white-modern-alone-story-book-cover-QHBKwQnsgzs.jpg',
+      title: 'Book Title 1',
+      author: 'Author Name 1',
+    },
+    {
+      cover: 'https://marketplace.canva.com/EAFf0E5urqk/1/0/1003w/canva-blue-and-green-surreal-fiction-book-cover-53S3IzrNxvY.jpg', // Placeholder image URL
+      title: 'Book Title 2',
+      author: 'Author Name 2',
+    },
+    {
+      cover: 'https://static01.nyt.com/images/2022/12/08/books/review/08NotableBookCovers-PROMO/08NotableBookCovers-PROMO-superJumbo.jpg', // Placeholder image URL
+      title: 'Book Title 3',
+      author: 'Author Name 3',
+    },
+  ];
+
 
   const handleFilterClick = () => {
     setShowFilter(!showFilter);
@@ -33,8 +55,17 @@ const Home = () => {
 
       {/* Page Content */}
       <div className="page-content">
-        <h1>Welcome to the Home</h1>
-        <p>This is your page content.</p>
+      <h1>Book Catalog</h1>
+      <div className="book-list">
+        {books.map((book, index) => (
+          <BookCard
+            key={index}
+            cover={book.cover}
+            title={book.title}
+            author={book.author}
+          />
+        ))}
+      </div>
       </div>
     </div>
   );
