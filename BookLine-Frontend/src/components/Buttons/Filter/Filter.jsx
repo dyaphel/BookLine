537eadb331a6564bc32a2bcf2b5f5b1a886dfa1d
CarloSegreto <carlosegreto95@gmail.com>
2025-03-12 17:39:40 +0100

    Filter as a Button
    - How should we do the filter option ?
      1 it lead to another page set the parameter and then get the differente result
    2 as a menu
    3 should we do it ???

---
+++ BookLine-Frontend/src/components/Buttons/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Filter.css'; // Import the CSS file for styling
+
+const Filter = () => {
+
+  return (
+    <div className="filter-container">
+      <button className="button-filter">
+        Filter
+      </button>
+    </div>
+
+  );
+};
+
+export default Filter;
--- BookLine-Frontend/src/components/Home/Home.jsx
+++ BookLine-Frontend/src/components/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import NavBar from "../Navbar/Navbar";
+import Filter from '../Buttons/Filter/Filter';
 const Home = ()=>{
 
     return (
-
-            <NavBar/>
-          
-    )
+            <div>
+              {/* Navbar */}
+              <NavBar />
+        
+              {/* Componente Filter */}
+              <Filter />
+        
+              {/* Contenuto della pagina */}
+              <div className="page-content">
+                <h1>Benvenuto nella Home</h1>
+                <p>Questo è il contenuto della tua pagina.</p>
+              </div>
+            </div>
+          );
 
 };
 export default Home;
