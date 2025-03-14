3a33c1ab3463af163528977abcdb2e9da5d6ad02
CarloSegreto <carlosegreto95@gmail.com>
2025-03-14 15:39:43 +0100

    login page style up

--- BookLine-Frontend/src/components/Login/Login.jsx
+++ BookLine-Frontend/src/components/Login/Login.jsx
@@ -12,27 +12,27 @@ const Login = () => {
     
 
     return (
-    <div className="login-page">
-        <h1 className="login-title">Login</h1>
-        <div className="login-container">
-            <form className="login-form">
+    <div className="mylogin-page">
+        <h1 className="mylogin-title">Login</h1>
+        <div className="mylogin-container">
+            <form className="mylogin-form">
                 <input
                     type="text"
                     placeholder="Username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    className="login-input" />
+                    className="mylogin-input" />
                 <input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="login-input" />
-                <button type="submit" className="login-button" disabled={loading}>
+                    className="mylogin-input" />
+                <button type="submit" className="mylogin-button" disabled={loading}>
                     {loading ? 'Loading...' : 'Login'}
                 </button>
             </form>
-            <Link to="/register" className="register-button-login">
+            <Link to="/register" className="myregister-button-login">
                 Register
             </Link>
         </div>
