374f48c1801fc44428121de800ba2d79ae72a1f4
dyaphel <carlosegreto95@gmail.com>
2025-07-28 21:51:03 +0200

    minor style changes on filter button and buttons dimension

--- backend/bookline/models.py
+++ backend/bookline/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class Book(models.Model):
     STATUS_CHOICES = [
-        ('available', 'Available'),
-        ('reserved', 'Reserved'),
-        ('checked_out', 'Checked Out'),
-        ('lost', 'Lost'),
+        ('available', 'Available'), #copies > reservations
+        ('reserved', 'Reserved'), # user made a reservation
+        ('not_available', 'not available'), # copies <= reservations
+        ('in_queue', 'In Queue'), # user is in queue for the book,
+        # ('lost', 'Lost'),
     ]
     # ISBN as the primary key
     isbn = models.CharField(max_length=13, primary_key=True)  # Assuming ISBN is a 13-digit number, use CharField
@@ -23,8 +24,9 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='book_covers/', null=True, blank=True)  # Store image files in 'book_covers/' folder
     genre = models.CharField(max_length=100, blank=True)
     language = models.CharField(max_length=50, blank=True)
+    # copies = models.PositiveIntegerField(blank = False, default=1)
+    # queue = models.PositiveIntegerField(default=0)  # Number of users in queue for the book
 
-    # add the status : available, not available, reserved;
     # queued user to the book;
     def __str__(self):
         return self.title
