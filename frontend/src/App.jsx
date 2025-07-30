0ef3ccd1b3eeebd40b09bb8dd910ecd6da58814a
dyaphel <carlosegreto95@gmail.com>
2025-07-30 20:38:27 +0200

    url correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import './App.css'
 import AppRoutes from './Routes/AppRoutes'
-import { AuthProvider } from './Utils/AuthContext'
+
 const App = () => {
 
   return (
     <>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <AuthProvider>
+
         <AppRoutes/>
-    </AuthProvider>
+
     </>
   )
 }
