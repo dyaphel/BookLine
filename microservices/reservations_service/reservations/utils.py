1c6115c7870cef982bf27bb422245ec1d54039bb
riccardomearelli <129194485+riccardomearelli@users.noreply.github.com>
2025-08-11 17:38:18 +0200

    Updated utils.py for reservations

--- microservices/reservations_service/reservations/utils.py
+++ microservices/reservations_service/reservations/utils.py
@@ -1,6 +1,19 @@
+from .models import Reservation
 import jwt
+import requests
 from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
+
+def create_reservation(user, book):
+    existing = Reservation.objects.filter(book=book, fulfilled=True).count()
+    if existing < book.available_copies:
+        # Immediate fulfillment
+        reservation = Reservation.objects.create(user=user, book=book, fulfilled=True, position=None)
+    else:
+        # Queueing
+        queue_position = Reservation.objects.filter(book=book).count() + 1
+        reservation = Reservation.objects.create(user=user, book=book, fulfilled=False, position=queue_position)
+    return reservation
 
 def get_token_from_request(request):
     auth_header = request.headers.get('Authorization')
